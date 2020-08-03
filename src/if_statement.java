9e1782853394e60185188e6f6675c5bd9b02f956
Esmira11 <mikailzadeh.esmira@gmail.com>
2020-08-02 23:03:21 -0400

    add if statement

---
+++ src/if_statement.java
@@ -0,0 +1,11 @@
+public class if_statement {
+    public static void main(String[] args) {
+        int a=9;
+        int b=3;
+        if (a>b){
+            System.out.println(a);
+        }else{
+            System.out.println(b);
+        }
+    }
+}
